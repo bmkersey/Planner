6cbb34e1f545dca8d7915d96ce58808170b18127
PickleRick <bmkersey@gmail.com>
2022-05-06 00:08:09 -0400

    set up check time to color code the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,49 @@
+var workDay =["9","10","11","12","13","14","15","16","17"]
 
 
+var checkTime = function(){
+    
+    var currentTime = moment().format("k")
+    currentTime= parseInt(currentTime)
+   
+    for (var i = 0; i <workDay.length;i++){
+
+        var workTime = parseInt(workDay[i])
+        if(currentTime === workTime){
+            $(".task"+workDay[i]).css("background-color","red")
+
+        }else if (currentTime > workTime){
+            $(".task" + workDay[i]).addClass("bg-secondary")
+            console.log("test")
+        }else{
+            $(".task"+workDay[i]).addClass("bg-success")
+            console.log("test")
+
+        }
+    }
 
 
+}
 
 var getDateP = function(){
     var currentDate = moment().format("dddd, MMMM Do")
-    console.log(currentDate)
+    
     $("#currentDay").text(currentDate)
 
+    checkTime();
+
 }
+
+var saveData = function(){}
+
+$(".container").click(function(event){
+
+    if (event.target.id === "save"){
+        saveData();
+    }
+    
+
+});
 
 
 
