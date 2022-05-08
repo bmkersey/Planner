4c03e9862027b9d7588fe29655554f3dab1c4daa
PickleRick <bmkersey@gmail.com>
2022-05-08 02:21:18 -0400

    enabled saving tasks to local storage and finished up styling. Also made it so when you click in the row you can change the task text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var workDay =["9","10","11","12","13","14","15","16","17"]
+var dailyTasks= []
+var loadedTasks = []
 
 
 var checkTime = function(){
@@ -10,13 +12,13 @@ var checkTime = function(){
 
         var workTime = parseInt(workDay[i])
         if(currentTime === workTime){
-            $(".task"+workDay[i]).css("background-color","red")
+            $(".task"+workDay[i]).addClass("present")
 
         }else if (currentTime > workTime){
-            $(".task" + workDay[i]).addClass("bg-secondary")
+            $(".task" + workDay[i]).addClass("past")
             console.log("test")
         }else{
-            $(".task"+workDay[i]).addClass("bg-success")
+            $(".task"+workDay[i]).addClass("future")
             console.log("test")
 
         }
@@ -25,21 +27,60 @@ var checkTime = function(){
 
 }
 
+var loadTasks = function(){
+    
+    var loadedTasks = localStorage.getItem("tasks");
+    var loadedTasks = JSON.parse(loadedTasks)
+
+    console.log(loadedTasks)
+
+    if (!loadedTasks){
+        return false;
+    } else {
+    
+        for (var i = 0; i < loadedTasks.length;i++){
+        var taskTime = loadedTasks[i].daypart;
+        var taskText= loadedTasks[i].task;
+        console.log(taskText);
+    
+        $("#task"+ taskTime).text(taskText);
+        }
+    }
+}
+
+
 var getDateP = function(){
     var currentDate = moment().format("dddd, MMMM Do")
     
     $("#currentDay").text(currentDate)
 
     checkTime();
+    loadTasks();
 
 }
 
-var saveData = function(){}
+var saveData = function(time){
+    var taskText= $("#task" + time).text()
+
+    var newTask= {
+        task:taskText,
+        daypart:time
+    }
+
+    dailyTasks.push(newTask)
+    localStorage.setItem("tasks", JSON.stringify(dailyTasks))
+
+}
 
 $(".container").click(function(event){
-
-    if (event.target.id === "save"){
-        saveData();
+    var timePart = event.target.id
+    var timePart = timePart.substring(4)
+    var textPart = event.target.id
+    var textPart = textPart.substring(0,4)
+    
+    if (textPart === "save"){
+        
+        saveData(timePart);
     }
     
 
